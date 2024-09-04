1700fd136e8a359f66e2c2f4c432160ecfcc8008
Alex Mason <77219320+amason445@users.noreply.github.com>
2024-09-03 20:05:51 -0600

    Wrote Pricing Calculators

---
+++ pricing/src/main/java/com/amason445/BinomialOptionsPricing.java
@@ -0,0 +1,33 @@
+package com.amason445;
+
+public class BinomialOptionsPricing 
+{
+    public static void main( String[] args )
+    {
+               // Parameters
+               int N = 1000; // Number of steps
+               double S0 = 100; // Initial stock price
+               double K = 100; // Strike price
+               double r = 0.05; // Risk-free rate
+               double sigma = 0.2; // Volatility
+               double T = 1.0; // Time to maturity in years
+       
+               // Create an instances of options prices
+               AmericanCallOption callPricer = new AmericanCallOption(N, S0, K, r, sigma, T);
+               AmericanPutOption  putPricer =  new AmericanPutOption(N, S0, K, r, sigma, T);
+               EuropeanCallOption euroCallPricer = new EuropeanCallOption(N, S0, K, r, sigma, T);
+               EuropeanPutOption euroPutPricer = new EuropeanPutOption(N, S0, K, r, sigma, T);
+       
+               // Calculate the option price
+               double callOptionPrice = callPricer.calculateOptionPremium();
+               double putOptionPrice = putPricer.calculateOptionPremium();
+               double euroCallOptionPrice  = euroCallPricer.calculateOptionPremium();
+               double euroPutOptionPrice = euroPutPricer.calculateOptionPremium();
+       
+               // Output the result
+               System.out.println("The price of the American call option is: " + callOptionPrice);
+               System.out.println("The price of the American put option is: " + putOptionPrice);
+               System.out.println("The price of the European call option is: " + euroCallOptionPrice);
+               System.out.println("The price of the European put option is: " + euroPutOptionPrice);
+    }
+}
---
+++ pricing/src/main/java/com/amason445/EuropeanPutOption.java
@@ -0,0 +1,46 @@
+package com.amason445;
+
+public class EuropeanPutOption {
+    private int N; //number of time steps in the model
+    private double s0; //spot price of security
+    private double K; //strike price of security
+    private double r; //risk free interest rate
+    private double sigma; //Volatility of security
+    private double T; //time in days until maturity
+
+    public EuropeanPutOption(int N, double s0, double K, double r, double sigma, double T) {
+        this.N = N;
+        this.s0 = s0;
+        this.K = K;
+        this.r = r;
+        this.sigma = sigma;
+        this.T = T;
+    }
+
+    public double calculateOptionPremium() {
+        double deltaT = T / N; //timestep interval
+        double u = Math.exp(sigma * Math.sqrt(deltaT)); //up movement
+        double d = 1 / u; //down movement
+        double p = (Math.exp(r * deltaT) - d) / (u - d); //risk neutral probability measure
+        double discountFactor = Math.exp(-r * deltaT);
+
+        //2d array to process model
+        double[][] option_values = new double[N +1][N +1];
+
+        //initialize stock prices and fill the array
+        for (int i = 0; i <= N; i++) {
+            double stockPriceAtMaturity = s0  * Math.pow(u, N - i) * Math.pow(d, i);
+            option_values[i][N] = Math.max(0, K - stockPriceAtMaturity);
+        }
+
+        //backward induction to calculate option value
+        for (int j = N - 1; j >= 0; j--) {
+            for (int i = 0; i <= j; i++) {
+                option_values[i][j] =  discountFactor * (p * option_values[i][j + 1] + (1 - p) * option_values[i + 1][j + 1]);
+                }
+            }
+
+        //price at the root node
+        return option_values[0][0]; 
+    } 
+}
