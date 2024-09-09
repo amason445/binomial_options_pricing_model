1e87c116de8549355d29f9a520debdec6492cc95
Alex Mason <77219320+amason445@users.noreply.github.com>
2024-09-08 18:07:56 -0600

    Added properties file and manager class

---
+++ pricing/src/main/java/com/amason445/TickerManager.java
@@ -0,0 +1,54 @@
+package com.amason445;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class TickerManager {
+
+    Properties tickerFeatures = new Properties();
+    String tickerName;
+
+    public TickerManager(String tickerName) {
+        this.tickerName = tickerName;
+    }
+
+    public void fillProperties() {
+        try (InputStream input = TickerManager.class.getClassLoader().getResourceAsStream(tickerName + ".properties")) {
+    
+            if (input == null) {
+                System.out.println("Sorry, unable to find " + tickerName + ".properties in project resources");
+                return;
+            }
+    
+            tickerFeatures.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    public double getSpotPrice() {
+        return getDoubleProperty("ticker.spot");
+    }
+
+    public double getStrikePrice() {
+        return getDoubleProperty("ticker.strike");
+    }
+
+    public double getSigma() {
+        return getDoubleProperty("ticker.sigma");
+    }
+
+    private double getDoubleProperty(String key) {
+        String value = tickerFeatures.getProperty(key);
+        if (value == null) {
+            throw new IllegalArgumentException("Property " + key + " not found in the properties file.");
+        }
+        try {
+            return Double.parseDouble(value);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid format for " + key + ": " + value);
+        }
+    }
+
+
+}
